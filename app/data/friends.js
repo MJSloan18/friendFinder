0e702d0d69cdf1f7bb33dbd76cd0dc3bd5598dc2
Michael Sloan <m.j.sloan88@gmail.com>
2017-12-12 17:31:50 +0000

    updated server.js file

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
 var friendList = [
 
     {
-        name: "Ahmed",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        name: "Derek Carr",
+        photo: "https://www.washingtonpost.com/rf/image_1484w/2010-2019/Wires/Images/2017-11-05/Getty/870695294.jpg?t=20170517",
         scores: [
             5,
             1,
@@ -17,8 +17,8 @@ var friendList = [
         ]
     },
     {
-        name: "Michael J. Sloan",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        name: "Marshawn Lynch",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcifrBmu-AECf4ZipPyE35gl_82tulLToYeyN99qqeGRTNclFtAg",
         scores: [
             3,
             5,
@@ -31,6 +31,42 @@ var friendList = [
             4,
             4
         ]
+    },
+
+    {
+        name: "Amari Cooper",
+        photo: "https://media.profootballfocus.com/2016/08/GettyImages-495353614.jpg?w=916&h=720",
+        scores: [
+            5,
+            2,
+            3,
+            1,
+            5,
+            4,
+            2,
+            3,
+            1,
+            4
+        ]
+
+    },
+
+    {
+        name: "Michael Crabtree",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7xwpG2Pw3P1x2WajA9jN-2kWBtjWMZC2_r0B-T39n5pCA_Hm-",
+        scores: [
+            2,
+            4,
+            4,
+            4,
+            3,
+            3,
+            3,
+            3,
+            3,
+            4
+        ]
+
     }
 
 ]
